13dc1c3a3b51ca6850ba75d9f5e82db57ea00264
git <weistable@outlook.com>
2024-11-07 20:14:05 +0800

    delete

--- home/cosmic/dotfiles/bat.nix
+++
@@ -1,8 +0,0 @@
-{
-  home.file.".config/bat/config".text = ''
-    --theme="Dracula"
-
-    # Show line numbers, Git modifications and file header (but no grid)
-    --style="numbers,changes,header"
-  '';
-}
